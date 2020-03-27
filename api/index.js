3ad3c0c62e88550615d07d461e05e1e7fe46798b
ramirobg94 <ramirobg94@gmail.com>
2020-03-27 19:31:32 +0100

    rename api constants due to a typo

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const fetch = require("./infra");
-const { API_ROUTES } = require("./contants");
+const { API_ROUTES } = require("./constants");
 
 const api = {
   login: async (context, { username, password }) =>
