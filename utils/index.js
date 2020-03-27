5cff454a442b7cf162ff9a069737d98830e6c37e
ramirobg94 <ramirobg94@gmail.com>
2020-03-27 19:45:33 +0100

    improve readability adding more and smaller functions

--- utils/index.js
+++ utils/index.js
@@ -1,16 +1,21 @@
+const extractLineItems = ({ invoice }) =>
+  (invoice &&
+    invoice.lineItems &&
+    Array.isArray(invoice.lineItems) &&
+    invoice.lineItems) ||
+  [];
+
+const computeLineItemAmount = ({ lineItem }) =>
+  (lineItem.quantity || 0) * (lineItem.unitPrice || 0);
+
+const computeTotalAmount = ({ invoice }) =>
+  extractLineItems({ invoice }).reduce(
+    (total, lineItem) => total + computeLineItemAmount({ lineItem }),
+    0
+  );
+
 const Invoice = {
-  computeTotalAmount: ({ invoice }) =>
-    (
-      (invoice &&
-        invoice.lineItems &&
-        Array.isArray(invoice.lineItems) &&
-        invoice.lineItems) ||
-      []
-    ).reduce(
-      (total, lineItem) =>
-        total + (lineItem.quantity || 0) * (lineItem.unitPrice || 0),
-      0
-    )
+  computeTotalAmount
 };
 
 module.exports = {
